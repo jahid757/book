c3d437d0cc869f7dfc6325afadaccbf427a0d923
Jahid <jahidhasan.developer@gmail.com>
2022-08-26 17:04:45 +0500

    update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ searchIcon.addEventListener('click', () => {
     const search = document.getElementById('search');
     search.classList.toggle('active');
 })
+const menu_id = document.getElementById('menu_icon');
+menu_id.addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active')
+})
